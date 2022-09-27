a7451520f8a46d7fdfc321058018f45dcf499da1
Shergotty <sebastian.hackler@gmx.de>
2022-09-27 12:58:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# PROJECT OVERVIEW
+
+## installation virtual environment
+
+1. To setup the necessary virtual environment (**venv**), run [`setup.ps1`](setup.ps1) in your powershell terminal.
+
+    ### open terminal:
+
+    ![](instructions\open_terminal.gif)
+    ![](https://github.com/Shergotty/PYTHON_BASE/blob/main/open_terminal.gif)
+
+If Python is not installed, Python version 3.10.7 will be downloaded ([Python](https://www.python.org/downloads/)) and the installer will be executed. Please install Python in order to setup the virtual environment and make sure you add your python installation folder to PATH (Checkbox at the end of installation). IF Python has to be installed, reboot your system in order to write your new systemvariable to your registry.
+
+### run [`setup.ps1`](setup.ps1) 
+
+```{ps}
+.\setup.ps1
+```
+
+![](instructions\install_requirements.gif)
+![](https://github.com/Shergotty/PYTHON_BASE/blob/main/install_requirements.gif)
+    
+## venv is activated
+
+![](instructions\ready.gif)
+![][](https://github.com/Shergotty/PYTHON_BASE/blob/main/ready.gif)
+
+---
+## uninstall virtual environment
+
+1. To unsintall the **venv**, run [`uninstall.ps1`](uninstall.ps1) in your powershell terminal.
+
+```{ps}
+.\uninstall.ps1
+```
---
+++ environment_scripts/setup_python_venv.ps1
@@ -0,0 +1,49 @@
+# redirect stderr into stdout
+$p = & { python -V } 2>&1
+$python_version = [System.Version]"3.10.7"
+$reference_version = [System.Version]"3.7.14"
+$version_number = if (!($p -is [System.Management.Automation.ErrorRecord])) {
+    [System.Version]($p -replace '\D+(\s+)', '$1')
+}
+
+function install_python {
+
+    $filename = "python-$python_version.exe"
+    $uri = "https://www.python.org/ftp/python/$python_version/$filename"
+    $outpath = "C:\Users\$env:UserName\Downloads\"    
+    $out = $outpath + $filename
+
+
+    Invoke-WebRequest -URI $uri -OutFile $out
+    Write-Output "please install python and restart setup.ps1 afterwards"
+    Start-Process $out -Wait -ArgumentList '/quiet', 'InstallAllUsers=0', 'PrependPath=1', 'InstallLauncherAllUsers=0'
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
+}
+
+function setup_venv {
+    if (!(Test-Path env:VIRTUAL_ENV)) {
+        try {
+            python -m venv .\venv
+        }
+        catch {
+            Write-Output "venv allready installed"
+        }
+    }
+    .\venv\Scripts\Activate
+    Write-Output "venv active"
+    Write-Output "venv installed in $env:VIRTUAL_ENV"
+}
+
+# check if an ErrorRecord was returned or [System.Version] is less than $reference_version
+if (!($p -is [System.Management.Automation.ErrorRecord] -or $version_number -lt $reference_version)) {
+    # return as is and run setup_venv
+    Write-Output $p" installed"
+    setup_venv
+}
+else {
+    # otherwise grab the version string from the error message and run install_python  
+    Write-Output $p.Exception.Message
+    install_python
+    setup_venv
+}

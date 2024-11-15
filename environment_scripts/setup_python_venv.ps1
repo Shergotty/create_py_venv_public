05ff5d846609d48a98465e1f57b507df8d0d19f4
Shergotty <sebastian.hackler@gmx.de>
2024-11-15 12:39:53 +0100

    reference_version update

--- environment_scripts/setup_python_venv.ps1
+++ environment_scripts/setup_python_venv.ps1
@@ -1,7 +1,7 @@
 # redirect stderr into stdout
 $p = & { python -V } 2>&1
 $python_version = [System.Version]"3.13.0"
-$reference_version = [System.Version]"3.11.0"
+$reference_version = [System.Version]"3.13.0"
 $version_number = if (!($p -is [System.Management.Automation.ErrorRecord])) {
     [System.Version]($p -replace '\D+(\s+)', '$1')
 }
